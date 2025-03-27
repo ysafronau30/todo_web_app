df1495c09c22713c1f01b704ec0aec73d566326b
ysafronau <esafronov313@gmail.com>
2025-03-28 01:05:06 +0300

    Implement 'Add a new todo to the list' feature

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ if not os.path.exists('todos.txt'):
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+
 st.title('My Todo App')
 st.subheader('This is my todo app')
 st.write('This app is to increase your productivity')
@@ -15,4 +22,4 @@ st.write('This app is to increase your productivity')
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='', placeholder='Add new todo')
+st.text_input(label='Todo', label_visibility="hidden", placeholder='Add new todo', on_change=add_todo, key='new_todo')
